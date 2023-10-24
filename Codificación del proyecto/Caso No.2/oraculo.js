7774e35a7d79493e80ffd1e1dd422c3c126ae452
Camilo Guerra <148283026+CamiloGuerra1@users.noreply.github.com>
2023-10-23 21:00:43 -0500

    Add files via upload

---
+++ Codificación del proyecto/Caso No.2/oraculo.js
@@ -0,0 +1,61 @@
+async function migracion_contrato(){
+    var fs = require('fs');
+    var Web3 = require('web3');
+    var solc = require('solc');
+    var sourceCode = await fs.readFileSync('contrato.sol').toString();
+    var compiledCode = await solc.compile(sourceCode, 1);
+    var compiledCodeContract = compiledCode.contracts[':contrato'];
+    var contratoInterface = await JSON.parse(compiledCodeContract.interface);
+    var contratoByteCode = compiledCode.contracts[':contrato'].bytecode;
+    var web3 = new Web3('http://127.0.0.1:7545');
+    var accounts = await web3.eth.getAccounts();
+    var contratoContract = new web3.eth.Contract(contratoInterface);
+    var myContract = await contratoContract.deploy({data: contratoByteCode}).send({from: accounts[1], gas: 4700000});
+    escuchar_proyecto(myContract, accounts);
+};
+
+function escuchar_proyecto(myContract, accounts){
+    var MYSQLEvents = require('mysql-events');
+    var conexion = {
+        host: 'localhost',
+        user: 'root',
+        password: 'camilo1012',
+    };
+    myCon = MYSQLEvents(conexion);
+    console.log('Conexión exitosa con la base de datos: Proyecto, gestionando la actividad para la tabla: controlvehicular\n');
+    console.log('Contrato desplegado en la red de Ganache, con la siguiente dirección: ',myContract.options.address,'\n');
+    var event1 = myCon.add(
+        'proyecto.controlvehicular',
+        function(oldRow, newRow, event) {
+            if (oldRow === null) {
+                var now = new Date();
+                var codigoParqueadero = newRow.fields['codigoParqueadero'].toString()
+                var identificadorCupoParqueadero = newRow.fields['identificadorCupoParqueadero'].toString();
+                var placaVehiculo = newRow.fields['placaVehiculo'].toString();
+                var estadoControlVehicular = newRow.fields['estadoControlVehicular'].toString();
+                var fecha = newRow.fields['fecha'].toString();
+                var hora = newRow.fields['hora'].toString();
+                myContract.methods.createControl(codigoParqueadero, identificadorCupoParqueadero, placaVehiculo, estadoControlVehicular,
+                    fecha, hora, String(now)).send({from: accounts[1], gas: 3000000}).on('transactionHash', function (hash) {
+                        //console.log('Transacción hash:', hash);
+                    })
+                    .on('receipt', function (receipt) { 
+                        //console.log('Recibo de transacción:', receipt);
+                    })
+                    .on('error', function (error) {
+                        console.error('Error en la transacción:', error);
+                    });
+
+                /*myContract.methods.createControl(newRow.fields['codigoParqueadero'].toString(), newRow.fields['identificadorCupoParqueadero'].toString(),
+                newRow.fields['placaVehiculo'].toString(), newRow.fields['estadoControlVehicular'].toString(), newRow.fields['fecha'].toString(), 
+                newRow.fields['hora'].toString(), String(now)).send({from: accounts[1], gas: 3000000});*/
+
+                console.log('\nEl registro para el ingreso/salida vehicular con la placa del vehiculo: ',newRow.fields['placaVehiculo'],
+                '\nha sido agreadado de forma exitosa, en la base de datos: ',newRow.database, ', para la tabla: ',newRow.table,
+                '\nCon fecha y hora: ', String(now));
+            }
+        }
+    );
+}
+
+migracion_contrato();
---
+++ Codificación del proyecto/Caso No.2/trazabilidad.js
@@ -0,0 +1,40 @@
+async function trazabilidadControlVehicular() {    
+    var {ethers} = require('ethers');
+    var direcion = 'http://127.0.0.1:7545';
+    var urlDireccion = new ethers.providers.JsonRpcProvider(direcion);
+    var contratoABI = require('./ContractABI.json');
+    var direccionContrato = '0x5BC1631FD57fc13f504445FFC7a1DDfd090EE360';
+    var senderDireccion = '0x7bf3035823290ca4eC5CB80195ABDd741da8E01C';
+    var contract = new ethers.Contract(direccionContrato, contratoABI, urlDireccion);
+    var gasPrice = await urlDireccion.getGasPrice();
+    var latestBlockNumber = await urlDireccion.getBlockNumber();
+    try {
+        var counterBigNumber = await contract.getCounter();
+        var counter = counterBigNumber.toNumber();
+        for (let i = 2; i < counter+1; i++){
+            var data = await contract.getControl(i, {from: senderDireccion}); 
+            var numeroBloque = await urlDireccion.getBlock(i);
+            console.log('Recepcionamiento de la data en archivo JSON:', data);
+
+            console.log('\nLa trazabilidad para el caso de estudio de gestión vehicular es:',
+            '\nCódigo del parqueadero: ', data.codigoParqueadero,
+            '\nIdentificador del cupo de parqueadero: ', data.identificadorCupoParqueadero,
+            '\nPlaca del vehiculo: ', data.placaVehiculo,
+            '\nEstado de la gestión vehicular: ', data.estadoControlVehicular,
+            '\nFecha de la gestión vehicular: ', data.fecha,
+            '\nHora de la gestión vehicular: ', data.hora), '\n';
+
+            console.log('\nInformación importante, referente a la transacción:',
+            `\nNúmero de bloque de la transacción: ${i}`,
+            '\nHash de la transacción: ', numeroBloque.hash,
+            '\nGas used/Gas usado en la transacción: ', numeroBloque.gasUsed.toString(),
+            '\nGas price/Precio del gas de la red de Ganache: ', gasPrice.toString(),
+            '\nGas limit/Limte de gas de la red de Ganache: ', numeroBloque.gasLimit.toString());
+        }
+    } catch (error){
+        console.log('El error es: ', error);
+    };
+    console.log('\nNúmero de bloques implementados en la red de Ganache:', latestBlockNumber);
+};
+
+trazabilidadControlVehicular();
