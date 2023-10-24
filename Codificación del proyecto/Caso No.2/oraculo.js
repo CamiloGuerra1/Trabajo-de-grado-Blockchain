9e5db772188e1affb481302e48c9b222f704d700
Camilo Guerra <148283026+CamiloGuerra1@users.noreply.github.com>
2023-10-23 21:04:08 -0500

    Update oraculo.js

--- Codificación del proyecto/Caso No.2/oraculo.js
+++ Codificación del proyecto/Caso No.2/oraculo.js
@@ -7,7 +7,7 @@ async function migracion_contrato(){
     var compiledCodeContract = compiledCode.contracts[':contrato'];
     var contratoInterface = await JSON.parse(compiledCodeContract.interface);
     var contratoByteCode = compiledCode.contracts[':contrato'].bytecode;
-    var web3 = new Web3('http://127.0.0.1:7545');
+    var web3 = new Web3('http://127.0.0.1:7545'); //Agregar la dirección de y puerto de la red de ganache (por medio de linea de comandos o software)
     var accounts = await web3.eth.getAccounts();
     var contratoContract = new web3.eth.Contract(contratoInterface);
     var myContract = await contratoContract.deploy({data: contratoByteCode}).send({from: accounts[1], gas: 4700000});
@@ -17,9 +17,9 @@ async function migracion_contrato(){
 function escuchar_proyecto(myContract, accounts){
     var MYSQLEvents = require('mysql-events');
     var conexion = {
-        host: 'localhost',
+        host: 'localhost', //Parametros de la base de datos
         user: 'root',
-        password: 'camilo1012',
+        password: '*******',
     };
     myCon = MYSQLEvents(conexion);
     console.log('Conexión exitosa con la base de datos: Proyecto, gestionando la actividad para la tabla: controlvehicular\n');
@@ -45,10 +45,6 @@ function escuchar_proyecto(myContract, accounts){
                     .on('error', function (error) {
                         console.error('Error en la transacción:', error);
                     });
-
-                /*myContract.methods.createControl(newRow.fields['codigoParqueadero'].toString(), newRow.fields['identificadorCupoParqueadero'].toString(),
-                newRow.fields['placaVehiculo'].toString(), newRow.fields['estadoControlVehicular'].toString(), newRow.fields['fecha'].toString(), 
-                newRow.fields['hora'].toString(), String(now)).send({from: accounts[1], gas: 3000000});*/
 
                 console.log('\nEl registro para el ingreso/salida vehicular con la placa del vehiculo: ',newRow.fields['placaVehiculo'],
                 '\nha sido agreadado de forma exitosa, en la base de datos: ',newRow.database, ', para la tabla: ',newRow.table,
