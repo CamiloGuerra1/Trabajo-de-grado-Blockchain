d1fe3ecfea817e42aec9405d1fba853686969822
Camilo Guerra <148283026+CamiloGuerra1@users.noreply.github.com>
2023-10-23 21:31:13 -0500

    Update trazabilidad.js

--- Codificación del proyecto/Caso No.1/trazabilidad.js
+++ Codificación del proyecto/Caso No.1/trazabilidad.js
@@ -1,22 +1,22 @@
 async function trazabilidadReserva() { 
     var {ethers} = require('ethers');
-    var direcion = 'http://127.0.0.1:7545';
+    var direcion = 'http://127.0.0.1:7545'; //Agregar la dirección y puerto de la red de ganache (por medio de linea de comandos o software)
     var urlDireccion = new ethers.providers.JsonRpcProvider(direcion);
-    var contratoABI = require('./ContractABI.json');
-    var direccionContrato = '0x3E97f9932313891DE86224D269Bd7D5693670d14';
-    var senderDireccion = '0xE2DeCC343ae71916d053276f5668B53333a9a961';
+    var contratoABI = require('./ContractABI.json'); //Lectra del contrato inteligente en formato ABI, previamente ya creado
+    var direccionContrato = '0x3E97f9932313891DE86224D269Bd7D5693670d14'; //Asignación de la dirección del contrato inteligente
+    var senderDireccion = '0xE2DeCC343ae71916d053276f5668B53333a9a961'; //Asignación del remitente del contrato inteligente
     var contract = new ethers.Contract(direccionContrato, contratoABI, urlDireccion);
     var gasPrice = await urlDireccion.getGasPrice();
     var numeroDeBloques = await urlDireccion.getBlockNumber();
     try {
-        var counterBigNumber = await contract.getCounter();
+        var counterBigNumber = await contract.getCounter(); //La función getCounter es obtenida por medio del contrato inteligente
         var counter = counterBigNumber.toNumber();
         for (let i = 2; i < counter+1; i++){
-            var informaciónDatos = await contract.getReserva(i, {from: senderDireccion});
+            var informaciónDatos = await contract.getReserva(i, {from: senderDireccion}); //La función getReserva es obtenida por medio del contrato inteligente
             var numeroBloque = await urlDireccion.getBlock(i);
             console.log('Recepcionamiento de la data en archivo JSON del caso de uso "Reserva": ', informaciónDatos);
 
-            console.log('\nLa traazabilidad para el caso de uso "Reserva" es:',
+            console.log('\nLa traazabilidad para el caso de uso "Reserva" es:', //Visualización de la información enviada
             '\nIdentificador de la reserva: ', informaciónDatos.identificadorReserva,
             '\nCódigo del parqueadero: ', informaciónDatos.codigoParqueadero,
             '\nIdentificador de cupo del parqueadero: ', informaciónDatos.identificadorCupoParqueadero,
@@ -25,7 +25,7 @@ async function trazabilidadReserva() {
             '\nHora de la reserva: ', informaciónDatos.horaReserva,
             '\nFecha y hora de la transacción: ', informaciónDatos.fechaHoraTransaccion);
 
-            console.log('\nInformación importante, referente a la transacción:',
+            console.log('\nInformación importante, referente a la transacción:', //Visualización de la información relacionada a la transacción generada
             `\nNúmero de bloque de la transacción: ${i}`,
             '\nHash de la transacción: ', numeroBloque.transactions, 
             '\nHash del bloque de la transacción: ', numeroBloque.hash,
