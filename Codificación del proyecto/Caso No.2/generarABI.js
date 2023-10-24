a73e0263a90ac97e80f1b7a68108fc8a6285a18d
Camilo Guerra <148283026+CamiloGuerra1@users.noreply.github.com>
2023-10-23 21:02:30 -0500

    Update generarABI.js

--- Codificación del proyecto/Caso No.2/generarABI.js
+++ Codificación del proyecto/Caso No.2/generarABI.js
@@ -1,15 +1,12 @@
 var fs = require('fs');
 var solc = require('solc');
 
-var contractPath = '/Users/camilo/proyecto2/contrato.sol'; 
+var contractPath = '/Users/user/proyecto2/contrato.sol'; //Agregar la dirección, donde se encuentra el contrato inteligente 
 var contractFile = fs.readFileSync(contractPath, 'utf-8'); 
 
-//Compila el contrato Solidity
 var compiledContract = solc.compile(contractFile, 1);
 
-//Obtiene el ABI del contrato compilado
 var abi = JSON.parse(compiledContract.contracts[':contrato'].interface);
 
-//Escribe el ABI en un archivo JSON
 var abiPath = './ContractABI.json';
 fs.writeFileSync(abiPath, JSON.stringify(abi));
