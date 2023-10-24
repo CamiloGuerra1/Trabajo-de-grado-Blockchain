e21e81c3f7f9e3485ffa4362f3886d4587ff42e2
Camilo Guerra <148283026+CamiloGuerra1@users.noreply.github.com>
2023-10-23 21:28:18 -0500

    Update oraculo.js

--- Codificación del proyecto/Caso No.1/oraculo.js
+++ Codificación del proyecto/Caso No.1/oraculo.js
@@ -7,7 +7,7 @@ async function migracion_contrato(){
     var compiledCodeContract = compiledCode.contracts[':contrato'];
     var contratoInterface = await JSON.parse(compiledCodeContract.interface);
     var contratoByteCode = compiledCode.contracts[':contrato'].bytecode;
-    var web3 = new Web3('http://127.0.0.1:7545');
+    var web3 = new Web3('http://127.0.0.1:7545'); //Agregar la dirección y puerto de la red de ganache (por medio de linea de comandos o software)
     var accounts = await web3.eth.getAccounts();
     var contratoContract = new web3.eth.Contract(contratoInterface);
     var myContract = await contratoContract.deploy({data: contratoByteCode}).send({from: accounts[0], gas: 4700000});
@@ -16,10 +16,10 @@ async function migracion_contrato(){
 
 function escuchar_proyecto(myContract, accounts){ 
     var MYSQLEvents = require('mysql-events');
-    var  conexion = {
+    var  conexion = { //Parametros de conexión a la base de datos
         host: 'localhost', 
         user: 'root', 
-        password: 'camilo1012', 
+        password: '*******', 
     };
     var myCon = MYSQLEvents(conexion);
     console.log('Conexión exitosa con la base de datos: Proyecto\nGestionando la actividad para la tabla: Reserva\n');
@@ -27,16 +27,16 @@ function escuchar_proyecto(myContract, accounts){
     var event1 = myCon.add(
         'proyecto.reserva',
         function(oldRow, newRow, event){
-            if(oldRow === null) {
-                var identificadorReserva = newRow.fields['identificadorReserva'].toString();
+            if(oldRow === null) { //Detección de inserciones en la base de datos
+                var identificadorReserva = newRow.fields['identificadorReserva'].toString(); //Conversión de información a cadena de caracteres
                 var codigoParqueadero = newRow.fields['codigoParqueadero'].toString();
                 var identificadorCupoParqueadero = newRow.fields['identificadorCupoParqueadero'].toString();
                 var placaVehiculo = newRow.fields['placaVehiculo'].toString();
                 var fechaReserva = newRow.fields['fechaReserva'].toString();
                 var horaReserva = newRow.fields['horaReserva'].toString();
-                var now = new Date();
-                myContract.methods.createReserva(identificadorReserva, codigoParqueadero, identificadorCupoParqueadero, placaVehiculo,
-                    fechaReserva, horaReserva, String(now)).send({from: accounts[0], gas: 3000000}).on('transactionHash', function (hash) {
+                var now = new Date(); //Obtener la fecha y hora actual del sistema operativo
+                myContract.methods.createReserva(identificadorReserva, codigoParqueadero, identificadorCupoParqueadero, placaVehiculo, 
+                    fechaReserva, horaReserva, String(now)).send({from: accounts[0], gas: 3000000}).on('transactionHash', function (hash) { //Envio de información a la cadena de blqoues de ganache
                         //console.log('Transacción hash:', hash);
                     })
                     .on('receipt', function (receipt) { 
@@ -49,16 +49,16 @@ function escuchar_proyecto(myContract, accounts){
                 '\nha sido agregado de forma exitosa, en la base de datos: ', newRow.database,', para la tabla: ', newRow.table,
                 '\nCon fecha y hora de registro: ', String(now)); 
             } 
-            if(oldRow !== null && newRow !== null) {
-                var identificadorReserva = newRow.fields['identificadorReserva'].toString();
+            if(oldRow !== null && newRow !== null) { //Detección de modificaciones en la base de datos
+                var identificadorReserva = newRow.fields['identificadorReserva'].toString(); //Conversión de información a cadena de caracteres
                 var codigoParqueadero = newRow.fields['codigoParqueadero'].toString();
                 var identificadorCupoParqueadero = newRow.fields['identificadorCupoParqueadero'].toString();
                 var placaVehiculo = newRow.fields['placaVehiculo'].toString();
                 var fechaReserva = newRow.fields['fechaReserva'].toString();
                 var horaReserva = newRow.fields['horaReserva'].toString();
-                var now = new Date();
+                var now = new Date(); //Obtener la fecha y hora actual del sistema operativo
                 myContract.methods.createReserva(identificadorReserva, codigoParqueadero, identificadorCupoParqueadero, placaVehiculo,
-                    fechaReserva, horaReserva, String(now)).send({from: accounts[0], gas: 3000000}).on('transactionHash', function (hash) {
+                    fechaReserva, horaReserva, String(now)).send({from: accounts[0], gas: 3000000}).on('transactionHash', function (hash) { //Envio de información a la cadena de blqoues de ganache
                         //console.log('Transacción hash:', hash);
                     })
                     .on('receipt', function (receipt) { 
